15d8ae8c452aeb9eb2104e25ccc849211e7a29aa
demon <597406711@qq.com>
2019-04-08 21:37:33 +0800

    3

---
+++ src/pages/playList.vue
@@ -0,0 +1,68 @@
+<template>
+    <!--正在播放-->
+    <div class="playList">
+        <ucraftMusicList :list="playlist"
+                    :listType="1"
+                    @select="selectItem"
+                    @del="deleteItem">
+            <div slot="listBtn"
+                 class="list-btn">
+                <span @click="$refs.dialog.show()">清空列表</span>
+            </div>
+        </ucraftMusicList>
+        <ucraftDialog ref="dialog"
+                   @confirm="clearList"
+                   bodyText="是否清空正在播放列表"
+                   confirmBtnText="清空" />
+    </div>
+</template>
+
+<script>
+    import {mapGetters, mapMutations, mapActions} from 'vuex'
+    import ucraftMusicList from 'components/musiclist/musiclist'
+    import ucraftDialog from 'base/udialog/udialog'
+    
+    export default {
+        name: "play-list",
+        data() {
+            return {
+                show: false,
+            }
+        },
+        components: {
+            ucraftMusicList,
+            ucraftDialog
+        },
+        computed: {
+            ...mapGetters([
+                'playing',
+                'playlist',
+                'currentMusic'
+            ])
+        },
+        methods: {
+            // 清空列表事件
+            clearList() {
+            },
+            // 播放暂停事件
+            selectItem(item, index) {
+            },
+            // 删除事件
+            deleteItem() {
+            },
+            ...mapMutations({
+                setPlaying: 'SET_PLAYING',
+                setCurrentIndex: 'SET_CURRENTINDEX',
+                clearPlaylist: 'CLEAR_PLAYLIST'
+            }),
+            ...mapActions([
+                'removerPlayListItem',
+                'clearPlayList'
+            ])
+        }
+    }
+</script>
+
+<style lang="less">
+  
+</style>
---
+++ src/pages/search.vue
@@ -0,0 +1,93 @@
+<template>
+    <!--搜索-->
+    <div class="search">
+        <ucraftLoading v-model="uLoadShow" />
+        <div class="search-head">
+            <span v-for="(item,index) in Artists.slice(0,5)"
+                  :key="index"
+                  @click="clickHot(item.first)">{{item.first}}</span>
+            <input class="search-input"
+                   type="text"
+                   placeholder="音乐/歌手"
+                   v-model.trim="searchValue"
+                   @keyup.enter="onEnter">
+        </div>
+        <ucraftMusicList ref="musicList"
+                    :list="list"
+                    :listType="2"
+                    @select="selectItem"
+                    @pullUp="pullUpLoad" />
+    </div>
+</template>
+
+<script>
+    import {mapGetters, mapActions, mapMutations} from 'vuex'
+    import {search, searchHot, getMusicDetail} from 'api'
+    import formatSongs from 'assets/js/song'
+    import ucraftLoading from 'base/uloading/uloading'
+    import ucraftMusicList from 'components/musiclist/musiclist'
+    import {loadMixin} from "assets/js/mixin";
+    
+    export default {
+        name: "search",
+        mixins: [loadMixin],
+        components: {
+            ucraftLoading,
+            ucraftMusicList
+        },
+        data() {
+            return {
+                Artists: [],//热搜数组
+                list: [],//搜索数组
+                page: 0,//分页
+                lockUp: true,//是否锁定上拉加载事件,默认锁定
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'playing',
+                'currentMusic',
+            ])
+        },
+        watch: {
+
+        },
+        created() {
+            // 获取热搜
+            searchHot()
+            .then()
+        },
+        methods: {
+            // 点击热搜
+            clickHot(name) {
+
+            },
+            // 搜索事件
+            onEnter() {
+               
+            },
+            //滚动加载事件
+            pullUpLoad() {
+             
+            },
+            //播放歌曲
+            async selectItem() {
+    
+            },
+            // 获取歌曲详情
+            _getMusicDetail() {
+
+            },
+            ...mapMutations({
+                setPlaying: 'SET_PLAYING'
+            }),
+            ...mapActions([
+                'selectAddPlay'
+            ])
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+   
+</style>
